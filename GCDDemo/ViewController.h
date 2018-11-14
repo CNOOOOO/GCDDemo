44c582e158c344e404424f2d5f6c8db67faa575c
Moker_C <33251546+CNOOOOO@users.noreply.github.com>
2018-11-14 13:40:33 +0800

    Initial Commit

---
+++ GCDDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCDDemo
+//
+//  Created by Mac2 on 2018/11/14.
+//  Copyright © 2018年 Mac2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GCDDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GCDDemo
+//
+//  Created by Mac2 on 2018/11/14.
+//  Copyright © 2018年 Mac2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
